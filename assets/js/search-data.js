4ac37816119663fb9c774e56eb96ecb4938f6b1d
hitesh-vs <99950365+hitesh-vs@users.noreply.github.com>
2025-01-24 09:37:49 +0000

    Deploying to gh-pages from @ hitesh-vs/hitesh-vs.github.io@4fc34e52353c6f22dcecc2d896cc3dcafff0d4a1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -450,9 +450,9 @@ ninja.data = [{
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-high-to-low-level-task-planning-and-execution-using-rl",
+          title: 'High to Low Level Task Planning and Execution using RL',
+          description: "An RL framework for mobile manipulators to learn navigation and grasping tasks sequentially for Household tasks",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
