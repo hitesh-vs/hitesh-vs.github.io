df55722d7458c6e35fb82c1ced2fbe6b944e7127
hitesh-vs <99950365+hitesh-vs@users.noreply.github.com>
2025-01-25 05:58:33 +0000

    Deploying to gh-pages from @ hitesh-vs/hitesh-vs.github.io@3a1e90ba6ceb2e7bde7a5e9eeda81a3ee1badf81 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -435,19 +435,19 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-",
+          title: '',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-adversarial-patch-generation-for-monocular-depth-networks",
+          title: 'Adversarial Patch Generation for Monocular Depth Networks',
+          description: "Customised implementation of this paper by Yamanaka et. al. to attack Depth networks with Adversarial patches.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-semantic-and-instance-segmentation-of-aerial-drone-imagery",
+          title: 'Semantic and Instance Segmentation of Aerial Drone Imagery',
+          description: "A custom CNN model inspired by ResNet-18 that can perform Semantics on footage obtained from drones.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-high-to-low-level-task-planning-and-execution-using-rl",
@@ -455,9 +455,9 @@ ninja.data = [{
           description: "An RL framework for mobile manipulators to learn navigation and grasping tasks sequentially for Household tasks",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-quadrotor-control-and-trajectory-optimization",
+          title: 'Quadrotor Control and Trajectory Optimization',
+          description: "Tracking complex trajectories of Quadrotors using PID and optimal controllers like LQR and MPC",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
